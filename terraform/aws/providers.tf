617588f772ce7df113a410d817300e271eaf6332
Nadav Nave <105583152+TheNadav@users.noreply.github.com>
2024-04-15 15:40:08 +0300

    Add vulnerabiity

--- terraform/aws/providers.tf
+++ terraform/aws/providers.tf
@@ -7,12 +7,12 @@ provider "aws" {
 provider "aws" {
   alias      = "plain_text_access_keys_provider"
   region     = "us-west-1"
-  access_key = "AKIAIOSFODNN7EXAMPLE"
-  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  access_key = "AKIAIOXXXXXX7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/XXXXXXX/bPxRfiCYEXAMPLEKEY"
 }
 
 terraform {
   backend "s3" {
-    encrypt = true
+    encrypt = false
   }
 }
